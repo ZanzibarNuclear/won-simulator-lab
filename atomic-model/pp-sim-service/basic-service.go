7ab3d98f0be940ebe8594eb284791c89c84758a1
Zanzibar <zanzisworld42@gmail.com>
2024-08-23 14:31:44 -0700

    Get basic REST service running.

---
+++ atomic-model/pp-sim-service/basic-service.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"slices"
+
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
+}
+
+var items []Item
+
+func main() {
+    router := mux.NewRouter()
+
+    // Define routes
+    router.HandleFunc("/items", getItems).Methods("GET")
+    router.HandleFunc("/items", createItem).Methods("POST")
+    router.HandleFunc("/items/{id}", getItem).Methods("GET")
+    router.HandleFunc("/items/{id}", deleteItem).Methods("DELETE")
+
+    // Start server
+    log.Println("Server is starting on port 8080...")
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getItems(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(items)
+}
+
+func getItem(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for _, item := range items {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(&Item{})
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+    var item Item
+    _ = json.NewDecoder(r.Body).Decode(&item)
+    items = append(items, item)
+    json.NewEncoder(w).Encode(item)
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    slices.DeleteFunc(items, func(item Item) bool {
+        return item.ID == params["id"]
+    })
+}
