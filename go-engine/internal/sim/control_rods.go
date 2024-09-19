a019c89bb0b730abde97b63f51f2d3ede685c53e
Zanzibar <zanzisworld42@gmail.com>
2024-09-18 19:21:02 -0700

    Another attempt; not quite there

--- go-engine/internal/sim/control_rods.go
+++ go-engine/internal/sim/control_rods.go
@@ -1,33 +1,29 @@
 package sim
 
-import (
-	"fmt"
-)
-
 const (
 	MAX_WITHDRAWAL_STEPS = 200
 	CONTROL_ROD_SPEED    = 40 // steps per minute
 )
 
 type ControlBank struct {
-	position         int // in steps
-	numRods          int
-	withdrawalTarget int // position to move toward
+	position int // in steps
+	numRods  int
 }
 
-func (cb *ControlBank) SetWithdrawalTarget(target int) {
-	fmt.Printf("Setting withdrawal target for bank to %d\n", target)
-	if target < 0 {
-		cb.withdrawalTarget = 0
-	} else if target > MAX_WITHDRAWAL_STEPS {
-		cb.withdrawalTarget = MAX_WITHDRAWAL_STEPS
-	} else {
-		cb.withdrawalTarget = target
-	}
-}
-
-func (cb *ControlBank) Withdrawn() bool {
+func (cb *ControlBank) FullyWithdrawn() bool {
 	return cb.position == MAX_WITHDRAWAL_STEPS
+}
+
+func (cb *ControlBank) FullyInserted() bool {
+	return cb.position == 0
+}
+
+func (cb *ControlBank) MoveTowardTarget(target int) {
+	if target > cb.position {
+		cb.position += min(CONTROL_ROD_SPEED, target-cb.position)
+	} else if target < cb.position {
+		cb.position -= min(CONTROL_ROD_SPEED, cb.position-target)
+	}
 }
 
 type ControlRods struct {
@@ -56,43 +52,21 @@ func NewControlRods() *ControlRods {
 
 func (cr *ControlRods) Update() {
 	if cr.withdrawShutdownBanks && !cr.ShutdownBanksFullyWithdrawn() {
-	Test1:
-		for i, bank := range cr.shutdownBanks {
-			if !bank.Withdrawn() {
-				fmt.Printf("Setting withdrawal target for bank %d\n", i)
-				cr.shutdownBanks[i].SetWithdrawalTarget(MAX_WITHDRAWAL_STEPS)
-				break Test1 // Only start withdrawing one bank at a time
+		for _, bank := range cr.shutdownBanks {
+			if !bank.FullyWithdrawn() {
+				bank.MoveTowardTarget(MAX_WITHDRAWAL_STEPS)
+				break // Only start withdrawing one bank at a time
 			}
 		}
 	}
 
 	if cr.insertShutdownBanks && !cr.ShutdownBanksFullyInserted() {
-		fmt.Printf("Inserting shutdown banks\n")
-		// Insert in reverse order from withdrawal
-	Test2:
 		for i := len(cr.shutdownBanks) - 1; i >= 0; i-- {
 			bank := cr.shutdownBanks[i]
-			if bank.position > 0 {
-				fmt.Printf("Setting withdrawal target for bank %d\n", i)
-				bank.SetWithdrawalTarget(0)
-				break Test2
+			if !bank.FullyInserted() {
+				bank.MoveTowardTarget(0)
+				break
 			}
-		}
-	}
-
-	for i, bank := range cr.controlBanks {
-		if bank.withdrawalTarget > bank.position {
-			cr.controlBanks[i].position += min(CONTROL_ROD_SPEED, bank.withdrawalTarget-bank.position) // Use index to update
-		} else if bank.withdrawalTarget < bank.position {
-			cr.controlBanks[i].position -= min(CONTROL_ROD_SPEED, bank.position-bank.withdrawalTarget)
-		}
-	}
-
-	for i, bank := range cr.shutdownBanks {
-		if bank.withdrawalTarget > bank.position {
-			cr.shutdownBanks[i].position += min(CONTROL_ROD_SPEED, bank.withdrawalTarget-bank.position)
-		} else if bank.withdrawalTarget < bank.position {
-			cr.shutdownBanks[i].position -= min(CONTROL_ROD_SPEED, bank.position-bank.withdrawalTarget)
 		}
 	}
 }
@@ -113,7 +87,7 @@ func (cr *ControlRods) InitiateShutdownBankInsertion() {
 
 func (cr *ControlRods) ShutdownBanksFullyWithdrawn() bool {
 	for _, bank := range cr.shutdownBanks {
-		if !bank.Withdrawn() {
+		if !bank.FullyWithdrawn() {
 			return false
 		}
 	}
@@ -122,7 +96,7 @@ func (cr *ControlRods) ShutdownBanksFullyWithdrawn() bool {
 
 func (cr *ControlRods) ShutdownBanksFullyInserted() bool {
 	for _, bank := range cr.shutdownBanks {
-		if bank.position > 0 {
+		if !bank.FullyInserted() {
 			return false
 		}
 	}
@@ -133,18 +107,16 @@ func (cr *ControlRods) Status() map[string]interface{} {
 	controlBanksStatus := make([]map[string]interface{}, len(cr.controlBanks))
 	for i, bank := range cr.controlBanks {
 		controlBanksStatus[i] = map[string]interface{}{
-			"controlBankNum":   i,
-			"position":         bank.position,
-			"withdrawalTarget": bank.withdrawalTarget,
+			"controlBankNum": i,
+			"position":       bank.position,
 		}
 	}
 
 	shutdownBanksStatus := make([]map[string]interface{}, len(cr.shutdownBanks))
 	for i, bank := range cr.shutdownBanks {
 		shutdownBanksStatus[i] = map[string]interface{}{
-			"shutdownBankNum":  i,
-			"position":         bank.position,
-			"withdrawalTarget": bank.withdrawalTarget,
+			"shutdownBankNum": i,
+			"position":        bank.position,
 		}
 	}
 
